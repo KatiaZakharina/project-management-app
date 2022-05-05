da41a51f8267d148aecae55b4bde62a0dbe6ade9
Katia <katia.zakharina@gmail.com>
2022-05-06 00:07:45 +0300

    feat: add custom mui theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
 
 import { App } from './App';
+import { theme } from 'styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/constants.ts
@@ -0,0 +1,12 @@
+export const WHITE = '#fff';
+export const BLACK = '#000';
+
+export const PURPLE = '#6930c3';
+export const VIOLET = '#5e60ce';
+export const AZURE_BLUE = '#5390d9';
+export const CYAN = '#48bfe3';
+export const TURQUOISE = '#64dfdf';
+
+export const RED = '#da1a1a';
+export const GREEN = '#2a9d8f';
+export const YELLOW = '#e9c46a';
