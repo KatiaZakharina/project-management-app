bb1acbbf1e25e0fd39ee05892ebd3c2aa1c92c66
Daria Litvinova <d.andreikovna@gmail.com>
2022-05-15 23:45:38 +0300

    feat: add confirmation modal

---
+++ src/components/ConfirmationModal/ConfirmationModal.styled.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { Box, Modal } from '@mui/material';
+
+import { WHITE, AZURE_BLUE } from 'styles/constants';
+import { rgba } from 'styles/helpers/rgba';
+
+export const StyledModal = styled(Modal)`
+  &.MuiModal-root {
+    background-color: ${rgba(AZURE_BLUE, 0.4)};
+  }
+`;
+
+export const StyledBox = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  padding: 20px;
+  display: flex;
+  gap: 10px;
+  transform: translate(-50%, -50%);
+  max-width: 400px;
+  background-color: ${WHITE};
+  border-radius: 10px;
+`;
