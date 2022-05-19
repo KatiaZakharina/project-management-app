6ee7d7ed972bb83fedbf9b6feb46d2c5bb759c08
Katia <katia.zakharina@gmail.com>
2022-05-19 11:46:17 +0300

    feat: add react-beautiful-dnd dependency

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,11 @@ import { theme } from 'styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
