a4c8a54e4ab0813d3bb9a5c1c2bee8f7f9dcd3a1
Daria Litvinova <d.andreikovna@gmail.com>
2022-05-30 13:52:07 +0300

    feat: add about-us

--- src/appConstants/index.ts
+++ src/appConstants/index.ts
@@ -1,9 +1,27 @@
 export const SERVER_URI: string = process.env.REACT_APP_SERVER_URI as string;
 
 export const TEAM_MATES = [
-  { name: 'Iryna Tabolich', link: 'https://github.com/irina-coud' },
-  { name: 'Daria Litvinova', link: 'https://github.com/andreikovna' },
-  { name: 'Katsiaryna Zakharyna', link: 'https://github.com/katiazakharina' },
+  {
+    id: 1,
+    name: 'Iryna Tabolich',
+    link: 'https://github.com/irina-coud',
+    src: 'https://avatars.githubusercontent.com/u/58627617?v=4',
+    text: 'Authorization, Localization, Tasks, Adaptive',
+  },
+  {
+    id: 2,
+    name: 'Daria Litvinova',
+    link: 'https://github.com/andreikovna',
+    src: 'https://avatars.githubusercontent.com/u/86688910?v=4',
+    text: 'Welcome page, Board page, Editing Titles',
+  },
+  {
+    id: 3,
+    name: 'Katsiaryna Zakharyna',
+    link: 'https://github.com/katiazakharina',
+    src: 'https://avatars.githubusercontent.com/u/74507403?v=4',
+    text: 'Backend deploy, Editing profile, Columns, DnD',
+  },
 ];
 
 export const LINK_TO_RSS = 'https://rs.school/react/';
